185f3d3c2d23554e73719a3bb1df5239aece7e8f
nketkar <npketkar@gmail.com>
2022-09-28 21:33:09 -0700

    working seperate decisino logic

---
+++ backend/decision.js
@@ -0,0 +1,38 @@
+/*
+Decision samples
+Coming Soon -> Decision samples based on:
+- age estimation
+- geographic location
+- token owenership
+- time expiring blocks
+- etc.
+ */
+
+function onePersonOneAccount(account) {
+    return account.numAccounts === 0;
+}
+
+function unlimitedAccountsUnlessBlocked(account) {
+    return account.hasBlockedAccounts === false;
+}
+
+function onePersonThreeAccountsUnlessBlocked(account) {
+    return account.numAccounts < 3 && account.hasBlockedAccounts === false;
+}
+
+function unlimitedAccountsUnlessBlockedInLastMonth(account) {
+    let timeSinceBlock = Date.now() - Date.parse(account.last_blocked_at);
+    let diffDays = Math.ceil(timeSinceBlock / (1000 * 60 * 60 * 24));
+
+    if (account.hasBlockedAccounts === true && diffDays > 30) {
+        return false;
+    }
+    return true;
+}
+
+export {
+    onePersonOneAccount,
+    unlimitedAccountsUnlessBlocked,
+    onePersonThreeAccountsUnlessBlocked,
+    unlimitedAccountsUnlessBlockedInLastMonth
+}
